5c2304433122802ae1f32e21f9b60a6f7e0cd7a8
jay-java <jayprajapati.tops@gmail.com>
2026-08-01 15:56:50 +0530

    jstl spring mvc

--- SpringMVC/src/main/java/com/controller/UserServlet.java
+++ SpringMVC/src/main/java/com/controller/UserServlet.java
@@ -1,20 +1,45 @@
 package com.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserServlet {
 
+	private final CustomerController customerController;
+
+	UserServlet(CustomerController customerController) {
+		this.customerController = customerController;
+	}
+
+//	@RequestMapping("/")
+//	public String indexPage(HttpServletRequest request) {
+//		request.setAttribute("num", 123);
+//		request.setAttribute("name", "user1");
+//		return "index";
+//	}
+
 	@RequestMapping("/")
-	public String indexPage() {
+	public String indexPage(Model m) {
+		m.addAttribute("num", 123);
+		m.addAttribute("name", "java");
+		List list = List.of("c", "java", "c++", "python");
+		m.addAttribute("list", list);
 		return "index";
 	}
-	
-	
+
 	@RequestMapping("/home")
 	public String homePage() {
 		return "home";
 	}
 
+	@RequestMapping("/register")
+	public String registerPage() {
+		return "register";
+	}
 }
--- SpringMVC/src/main/webapp/WEB-INF/jsp/index.jsp
+++ SpringMVC/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -1,5 +1,30 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ page isELIgnored="false"%>
+<!DOCTYPE html>
 <html>
 <body>
-<h2>Hello World!</h2>
+	<h2>Hello World!</h2>
+	<h1>
+		<a href="register">Register</a>
+	</h1>
+	<%-- <%
+	int num = (Integer) (request.getAttribute("num"));
+	String name = (String) request.getAttribute("name");
+	%>
+	<%
+	out.print(num);
+	out.print(name);
+	%> --%>
+
+	<h1>Num : ${num }</h1>
+	<h1>Name : ${name }</h1>
+	<h1>List : ${list }</h1>
+
+	<c:forEach items="${list }" var="i">
+		<h1>${i }</h1>
+	</c:forEach>
+
 </body>
 </html>
